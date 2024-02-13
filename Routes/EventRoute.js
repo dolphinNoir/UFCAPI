d757a72f48325b0a26ce21517295b56241501c73
johnnybasgallop04 <106543268+johnnybasgallop04@users.noreply.github.com>
2024-02-12 18:18:00 -0800

    added in the first version of the Find events by fighter name endpoint

--- Routes/EventRoute.js
+++ Routes/EventRoute.js
@@ -8,5 +8,21 @@ router.get("/", (req,res) => {
     res.json("Welcome to the Events Endpoint")
 })
 
+router.get("/FindEventsByFighterName/:FighterName", async (req, res) => {
+    try {
+        let fighterName = req.params.FighterName
+        let events = await UFCModel.find({
+              $or: [
+            { "Fighter 1": fighterName },
+            { "Fighter 2": fighterName }
+        ]})
+
+        res.json(events)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 module.exports = router
--- Routes/FighterRoute.js
+++ Routes/FighterRoute.js
@@ -8,21 +8,6 @@ router.get("/", (req,res) => {
     res.json("Welcome to the Fighters Endpoint")
 })
 
-router.get("/FindStatsByFighterName/:FighterName", async (req, res) => {
-    try {
-        let fighterName = req.params.FighterName
-        let stats = await UFCModel.find({
-              $or: [
-            { "Fighter 1": fighterName },
-            { "Fighter 2": fighterName }
-        ]})
-
-        res.json(stats)
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
 
 
 
